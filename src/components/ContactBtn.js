7cd3a0571c9132c62da835e26913eb80cc28d414
vladazachem <71698265+vladazachem@users.noreply.github.com>
2025-01-08 17:27:36 +0100

    Add ContactBtn component for navigation links

---
+++ src/components/ContactBtn.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function ContactBtn({ title, url }) {
+  return (
+    <div>
+      <Link
+        href={url}
+        className="text-typo-main hover:text-btns-hover flex items-center gap-2"
+      >
+        <p
+            className="text-typo-secondary hover:text-btns-hover font-code text-btns uppercase"
+        >
+            {title}
+        </p>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="10"
+            height="10"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="inline-block"
+          >
+            <path d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"/>
+          </svg>
+      </Link>
+    </div>
+  );
+}
