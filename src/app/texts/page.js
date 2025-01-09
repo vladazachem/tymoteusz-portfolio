f9a0e86ddd1df14259970ca4c9c8efcf36723914
vladazachem <71698265+vladazachem@users.noreply.github.com>
2025-01-09 16:25:20 +0100

    Refactor AllTextsPage layout for improved structure and styling

--- src/app/texts/page.js
+++ src/app/texts/page.js
@@ -3,9 +3,9 @@ import ProjectCard from "../../components/ProjectCard";
 
 export default function AllTextsPage() {
   return (
-    <div className="">
-      <h1 className="text-h1 font-sans mb-8">Teksty</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="flex flex-col w-full gap-10">
+      <h1 className="text-h3 text-center font-sans font-medium">Teksty</h1>
+      <div className="grid grid-cols-2 gap-6">
         {sampleData.texts.map((text) => (
           <ProjectCard
             key={text.id}
