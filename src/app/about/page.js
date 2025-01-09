35821ab00acea3025a40f7ae1fcfd3610c830ffe
vladazachem <71698265+vladazachem@users.noreply.github.com>
2025-01-09 16:24:03 +0100

    Update typography in About and NotFound components for improved readability

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className="flex items-center">
       <div className="grid grid-cols-2">
-      <h1 className="text-h1 font-sans col-start-1 row-start-1">O mnie</h1>
+      <h1 className="text-h2 font-medium font-sans col-start-1 row-start-1">O mnie</h1>
       <div className="text-p col-start-2 row-start-2">
         {sampleData.about.content.map((content) => (
         <p key={content} className="text-p">{content}</p>
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div className="w-screen">
       <div className="flex flex-col items-center justify-center text-center">
-        <h1 className="text-h1">404</h1>
+        <h1 className="text-h1 font-medium">404</h1>
         <p className="text-p mt-4">
           Ups! Strona, której szukasz, nie istnieje.
         </p>
