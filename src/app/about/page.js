d2cf022507f9d9705a39d84b578b7cdf502ffd1b
vladazachem <71698265+vladazachem@users.noreply.github.com>
2025-01-09 15:24:15 +0100

    Add some content in about section and create 2x2 grid system in about me section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,9 +2,13 @@ import { sampleData } from "../../data/sampleData";
 
 export default function About() {
   return (
-    <div className="p-5 sm:p-10">
-      <h1 className="text-h1 font-sans">O mnie</h1>
-      <p className="text-p mt-4 sm:mt-10">{sampleData.about.content}</p>
+    <div className="grid grid-cols-2">
+      <h1 className="text-h1 font-sans col-start-1 row-start-1">O mnie</h1>
+      <div className="text-p col-start-2 row-start-2">
+        {sampleData.about.content.map((content) => (
+        <p key={content} className="text-p">{content}</p>
+      ))}
+      </div>
     </div>
   );
 }
--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -30,7 +30,11 @@ export const sampleData = {
     },
   ],
   about: {
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    content: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel magna est. Mauris commodo nisi in erat tincidunt vestibulum. Sed maximus tincidunt sapien, in mollis erat pellentesque non. Pellentesque porta ac felis et ultricies. Phasellus aliquam neque quis augue scelerisque bibendum ac quis nisl.",
+      ["Integer velit nisi, tempor ac odio sit amet, ultricies feugiat leo. Ut dapibus leo ligula, ultrices sollicitudin dui porta ac. Donec finibus metus erat, vel aliquet elit semper scelerisque. Nunc id iaculis velit. Proin sit amet magna erat. Sed et nibh eu urna pretium posuere ut at mi."],
+      ["Sed viverra iaculis nulla, sed finibus risus malesuada eget. Vivamus pharetra elementum nisl sit amet sollicitudin. Donec libero libero, tincidunt placerat pulvinar eu, ultrices vel urna. Sed id nisl rhoncus, aliquam nisi quis, elementum velit."],
+      ["Donec ut rhoncus nibh, vel molestie quam. Quisque sit amet euismod sem. Suspendisse varius ante et tempus feugiat. Mauris dolor leo, suscipit at ultrices mollis, laoreet sit amet turpis. Quisque suscipit scelerisque erat, ut imperdiet ante euismod eu."],
+    ],
     contact: [
       {
         title: "Instagram",
