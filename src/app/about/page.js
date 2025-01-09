11ca70bffc03a0e393a9d748528f805057d3ce07
vladazachem <71698265+vladazachem@users.noreply.github.com>
2025-01-09 16:17:57 +0100

    Refactor layout and components for improved styling and alignment

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,13 +2,15 @@ import { sampleData } from "../../data/sampleData";
 
 export default function About() {
   return (
-    <div className="grid grid-cols-2">
+    <div className="flex items-center">
+      <div className="grid grid-cols-2">
       <h1 className="text-h1 font-sans col-start-1 row-start-1">O mnie</h1>
       <div className="text-p col-start-2 row-start-2">
         {sampleData.about.content.map((content) => (
         <p key={content} className="text-p">{content}</p>
       ))}
       </div>
+    </div>
     </div>
   );
 }
