55fe619dd08bdfaa1823454d5ba71c76dfdf8db2
vladazachem <71698265+vladazachem@users.noreply.github.com>
2025-01-09 17:21:55 +0100

    Enhance NotFound component layout for better centering and alignment

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="w-screen">
+    <div className="w-screen flex justify-center">
       <div className="flex flex-col items-center justify-center text-center">
         <h1 className="text-h1 font-medium">404</h1>
         <p className="text-p mt-4">
