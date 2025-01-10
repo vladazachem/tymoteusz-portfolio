0f4e199bcea1ebdb1f2379bfd4ef13852a48e250
vladazachem <71698265+vladazachem@users.noreply.github.com>
2025-01-10 16:17:39 +0100

    Enhance ProjectCard component with improved layout, styling adjustments, and updated image overlay for better visual appeal

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,8 +3,12 @@ import Link from "next/link";
 
 export default function ProjectCard({ title, category, description }) {
   return (
-    <Link href={`/texts/${category.toLowerCase()}/${encodeURIComponent(title.toLowerCase())}`}>
-      <div className="bg-bg-lines p-5 shadow-md hover:shadow-lg transition-shadow duration-300">
+    <Link
+      href={`/texts/${category.toLowerCase()}/${encodeURIComponent(
+        title.toLowerCase()
+      )}`}
+    >
+      <div className="p-10 flex flex-col gap-4 border-solid border-[2px] border-gray-300 hover:border-gray-950 group transition-colors duration-300">
         <div className="relative w-full h-0 pb-[40%]">
           <Image
             src="/the-great-wave-off-kanagawa.jpg"
@@ -12,11 +16,15 @@ export default function ProjectCard({ title, category, description }) {
             layout="fill"
             objectFit="cover"
           />
-          <div className="absolute inset-0 bg-black/30 group-hover:opacity-0 transition-opacity duration-300"></div>
+          <div className="absolute inset-0 bg-black/40 opacity-100 group-hover:opacity-0 transition-opacity duration-300"></div>
         </div>
-        <h2 className="text-h3 font-sans mt-4">{title}</h2>
-        <p className="text-typo-secondary text-sm mt-1">{category}</p>
-        <p className="line-clamp-3 leading-7">{description}</p>
+        <div className="flex flex-col">
+          <h2 className="text-h3 font-sans font-medium">{title}</h2>
+          <p className="text-typo-secondary text-h6 font-code font-medium">
+            {category}
+          </p>
+        </div>
+        <p className="line-clamp-3 leading-6 text-p">{description}</p>
       </div>
     </Link>
   );
